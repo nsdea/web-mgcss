90ec32466b8ce0c3410640de87e695e4506a99aa
nsde <neostyxde@gmail.com>
2021-12-20 23:35:20 +0100

    Fixed everything

--- web.py
+++ web.py
@@ -6,9 +6,21 @@ app = flask.Flask(__name__)
 def default_rendering():
     return 
 
-for page in os.listdir('public/templates'):
-    if page.startswith('__'):
-        app.add_url_rule(page.replace('.html', '').replace('__', '/'), page, lambda page=page: flask.render_template(page) , methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
+
+@app.route('/<a>')
+def layer_a(a):
+    return flask.render_template(f'{a}.html')
+
+@app.route('/<a>/<b>')
+def layer_b(a, b):
+    return flask.render_template(f'{a}/{b}.html')
+
+@app.route('/<a>/<b>/<c>')
+def layer_c(a, b, c):
+    return flask.render_template(f'{a}/{b}/{c}.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2021, debug=True)
